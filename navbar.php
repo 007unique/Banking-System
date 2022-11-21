ab4ec73277c58d233286287a1b8a6a18dbcc82f4
Unique <patelunique1@gmail.com>
2022-11-21 15:23:02 +0530

    Third

--- navbar.php
+++ navbar.php
@@ -1,6 +1,6 @@
 <!-- navbar -->
 <nav class="navbar navbar-expand-md">
-  <a class="navbar-brand" href="index.php">THE SPARKS BANK</a>
+  <a class="navbar-brand" href="index.php">INDIAN BANK</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
     <span class="navbar-toggler-icon"></span>
   </button>
